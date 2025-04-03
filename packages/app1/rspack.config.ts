ccefe61593bcbf5d7728ef4df93988e133187054
eswar3940 <eswar3940@gmail.com>
2025-04-03 17:23:42 +0530

    Ys

--- packages/app1/rspack.config.ts
+++ packages/app1/rspack.config.ts
@@ -5,7 +5,7 @@ import baseConfig from './module-federation.config';
 export default function (pluginOptions: any = {}, context: any = {}) {
   // ✅ Ensure context.project.root is defined
   context.project ??= {};
-  context.project.root = context.project.root || __dirname.replace(/\\/g, '/').split('/packages/')[1].split('/')[0];
+  context.project.root = process.cwd()||context.project.root || __dirname.replace(/\\/g, '/').split('/packages/')[1].split('/')[0]||{};
 
   context.options ??= {};
   context.options.output ??= { publicPath: 'auto' };
